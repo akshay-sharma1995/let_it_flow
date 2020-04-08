05e51345cd4e97077e05a8973820f0e972db2685
Stam <sathinio@andrew.cmu.edu>
2020-04-07 23:57:09 -0400

    Updated

--- src/main.py
+++ src/main.py
@@ -3,25 +3,42 @@ import pdb
 import argparse
 import torch
 import losses
-from utils import *
+# # from utils import *
 
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def main(args):
-    args = parse_args()
+def parse_arguments():
+# Command-line flags are defined here.
+        parser = argparse.ArgumentParser()
+
+        parser.add_argument('--num_epochs', dest='num_epochs', type=int,
+                                                default=100, help="Number of epochs")
+
+        parser.add_argument("--lr_disc",dest="lr_disc",type=float,
+                                                default=5e-4,help="Discriminator Learning Rate")
+
+        parser.add_argument('--lr_gen', dest='lr_gen', type=float,
+                                                default=5e-4, help="Generator Learning Rate")
+        return parser.parse_args()
+
+def main():
+    args = parse_arguments()
     
     lr_disc = args.lr_disc
     lr_gen = args.lr_gen
     num_epochs = args.num_epochs
 
-    ### TODO:
-    # make directories
-    # make network instances
-    # load checkpoints if needed
-    # initialize all props to be saved
-    # train function
-    # test function
+    print('num_epochs')
+
+#     ### TODO:
+#     # make directories
+#     # make network instances
+#     # load checkpoints if needed
+#     # initialize all props to be saved
+#     # train function
+#     # test function
 
 
-if __name__=="__main":
-    main(sys.argv)
+if __name__ == "__main__":
+    main()
+

5e4f3765ee7627f317ac018279697b7b4af33632
Muhammad441 <muhammadsuhail441@gmail.com>
2020-04-15 15:37:38 -0400

    Data loader with random crop

--- src/DataLoader.py
+++ src/DataLoader.py
@@ -74,17 +74,39 @@ class ToTensor(object):
         return {'frame1':torch.from_numpy(sample['frame1']), 
                 'frame2':torch.from_numpy(sample['frame2'])}
 
+
+class RandomCrop(object):
+    def __init__(self, size):
+        self.new_h = size[0]
+        self.new_w = size[1]
+    def __call__(self, sample):
+        frame1, frame2 = sample['frame1'], sample['frame2']
+        h, w = frame1.shape[:2]
+
+        # print("h,w ",h,w)
+        # print("new_h,new_w ",self.new_h,self.new_w)
+        top = np.random.randint(0, h - self.new_h)
+        left = np.random.randint(0, w - self.new_w)
+
+        frame1 = frame1[top: top + self.new_h,
+                        left: left + self.new_w]
+
+        frame2 = frame2[top: top + self.new_h,
+                        left: left + self.new_w]
+
+        return {'frame1': frame1, 'frame2': frame2}
+
 def main():
     dataset = KITTIDataset(folder_name='../data_scene_flow_multiview/training/image_2/',
-    transform=transforms.Compose([
+    transform=transforms.Compose([RandomCrop([320, 896]),
         Normalize(),
         ToTensor()
     ]
     ))
 
-    # sample = dataset[0]
+    dataloader = DataLoader(dataset, batch_size = 20, shuffle = True, num_workers = 4)
 
-    dataloader = DataLoader(dataset, batch_size = 4, shuffle = True, num_workers = 4)
-
+    for batch_ndx, sample in enumerate(dataloader):
+        print(sample['frame1'].shape)
 if __name__ == "__main__":
     main()

d2eaced4580f64ef54c0d7271b7160a0ea358da5
katigoshvadi <43680795+katigoshvadi@users.noreply.github.com>
2020-04-16 01:00:50 -0400

    Add files via upload

--- src/dataset.py
+++ src/dataset.py
@@ -5,28 +5,30 @@ import os
 
 class image_pair_dataset(Dataset):
 
-    def__init__(self, root_dir, transform=None, channels_first=False):
-        """
-        Args:
-            root_dir (string): Directory with all the image pairs
-            transform (callable, optional): Optional transform to be applied on a sample
-            channels_first (bool): Swap the channels axis to index 0
-        """
+  def __init__(self, root_dir, transform=None, channels_first=False)):
 
-        self.root_dir = root_dir
-        self.transform = transform
-        self.channels_first = channels_first
+  # def __init__(self, root_dir, transform=None, channels_first=False):
+  """
+  Args:
+  root_dir (string): Directory with all the image pairs
+  transform (callable, optional): Optional transform to be applied on a sample
+  channels_first (bool): Swap the channels axis to index 0
+  """
 
-    def __len__(self):
-        return len(self.data)
+    self.root_dir = root_dir
+    self.transform = transform
+    self.channels_first = channels_first
 
-    def __get_item__(self,idx):
-       file_name = os.path.join(self.root_dir, "{}.npy".format(idx)) 
-       sample = np.load(file_name)['arr_0']
-       if(self.transform):
-           sample = self.transform(sample)
-           if(self.channels_first):
-               sample.permute(0,3,1,2)
+  def __len__(self):
+    return len(self.data)
 
-    
-       return sample
+  def __get_item__(self,idx):
+    file_name = os.path.join(self.root_dir, "{}.npy".format(idx)) 
+    sample = np.load(file_name)['arr_0']
+    if(self.transform):
+      sample = self.transform(sample)
+    if(self.channels_first):
+      sample.permute(0,3,1,2)
+
+
+    return sample

7efe5c3fd82c8d7ef03814175d7d31c5991f70b0
katigoshvadi <43680795+katigoshvadi@users.noreply.github.com>
2020-04-16 19:57:30 -0400

    Add files via upload

--- src/generator.py
+++ src/generator.py
@@ -7,7 +7,7 @@ import sys
 
 
 class gen(nn.Module):
-    def __init__(self, input_channels= 2, latent_dim=108, out_channels=2, lr=1e-3):
+    def __init__(self, input_channels= 2, latent_dim=108, out_channels=2, lrEnc=1e-3, lrDec=1e-3):
         
         '''
         input_channels: number of channels in the input image
@@ -15,6 +15,51 @@ class gen(nn.Module):
         out_channels: number of channels in the optical flow (will be 2, 1 for each axis)
         '''
         super(gen, self).__init__()
+
+        self.input_channels = input_channels
+        self.latent_dim = latent_dim
+        self.out_channels = out_channels
+        self.lrEnc = lrEnc
+        self.lrDec = lrDec
+
+        self.Encoder = Encoder(self.input_channels, self.latent_dim, self.out_channels, self.lrEnc)
+        self.Decoder = Decoder(self.Encoder.encoded_dim, self.input_channels, self.latent_dim, self.out_channels, self.lrDec)
+
+    def sample_noise(self, mean, logvar):
+        tau = torch.randn_like(logvar).to(logvar.device)
+        std = torch.exp(0.5*logvar)
+
+        return (mean + tau*std)
+
+
+    def forward(self, x):
+        '''
+        x: (batch_size, 2*channels, height, width)
+            Each sample in the batch is a preprocessed pair of 2 consecutive images of dim (channels, height, width)
+        '''
+        height = x.shape[2]
+        width = x.shape[3]
+
+        encoded_vec = self.Encoder(x)
+        encoded_vec = encoded_vec.reshape(encoded_vec.shape[0],-1)
+        noise_params = self.Encoder.latent_code(encoded_vec)
+        mean, logvar = noise_params[:,0:self.latent_dim], noise_params[:,self.latent_dim:]
+        sample = self.sample_noise(mean, logvar)
+        
+        decoded_latent_code = self.Decoder.latent_code_decoder(sample)
+        decoded_latent_code = decoded_latent_code.reshape(decoded_latent_code.shape[0],16, 9, 27)
+
+        optical_flow = self.Decoder(decoded_latent_code)
+
+        optical_flow = 5 * torch.tanh(optical_flow)
+
+        return optical_flow
+
+class Encoder(nn.Module):
+
+    def __init__(self, input_channels= 2, latent_dim=108, out_channels=2, lr=1e-3):
+
+        super(Encoder, self).__init__()
 
         self.input_channels = input_channels
         self.latent_dim = latent_dim
@@ -40,73 +85,61 @@ class gen(nn.Module):
                                     nn.BatchNorm2d(16),
                                     )
 
-        encoded_dim = 16 * 27 * 9
+        self.encoded_dim = 16 * 27 * 9
 
         self.latent_code = nn.Sequential(
-                                        nn.Linear(in_features=encoded_dim, out_features=encoded_dim//9, bias=True),
+                                        nn.Linear(in_features= self.encoded_dim, out_features=self.encoded_dim//9, bias=True),
                                         nn.ReLU(),
-                                        nn.BatchNorm1d(encoded_dim//9),
+                                        nn.BatchNorm1d(self.encoded_dim//9),
                                         
-                                        nn.Linear(in_features=encoded_dim//9, out_features=2*latent_dim, bias=True),
+                                        nn.Linear(in_features=self.encoded_dim//9, out_features=2*latent_dim, bias=True),
                                         )
 
-        self.latent_code_decoder = nn.Sequential(
-                                                nn.Linear(in_features=latent_dim, out_features=encoded_dim, bias=True),
-                                                nn.ReLU(),
-                                                )
-
-        self.decoder = nn.Sequential(
-                                    nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=3, stride=2),
-                                    nn.ReLU(),
-                                    nn.BatchNorm2d(16),
-                                    nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=2),
-                                    nn.ReLU(),
-                                    nn.BatchNorm2d(8),
-
-                                    nn.ConvTranspose2d(in_channels=8, out_channels=8, kernel_size=3, stride=2),
-                                    nn.ReLU(),
-                                    nn.BatchNorm2d(8),
-                                    nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=3, stride=2),
-                                    nn.ReLU(),
-                                    nn.BatchNorm2d(4),
-
-                                    nn.ConvTranspose2d(in_channels=4, out_channels = 4, kernel_size=2, stride=2),
-                                    nn.ReLU(),
-                                    nn.BatchNorm2d(4),
-                                    nn.ConvTranspose2d(in_channels=4, out_channels = 2, kernel_size=3, stride=1),
-                                    )
-        
-        self.optimizer = torch.optim.Adam(self.parameters(), lr=lr)
-
-    def sample_noise(self, mean, logvar):
-        tau = torch.randn_like(logvar).to(logvar.device)
-        std = torch.exp(0.5*logvar)
-
-        return (mean + tau*std)
-
+        self.Encoptimizer = torch.optim.Adam(self.parameters(), lr=lr)
 
     def forward(self, x):
-        '''
-        x: (batch_size, 2*channels, height, width)
-            Each sample in the batch is a preprocessed pair of 2 consecutive images of dim (channels, height, width)
-        '''
-        height = x.shape[2]
-        width = x.shape[3]
+        return self.encoder(x)
 
-        encoded_vec = self.encoder(x)
-        encoded_vec = encoded_vec.reshape(encoded_vec.shape[0],-1)
-        noise_params = self.latent_code(encoded_vec)
-        mean, logvar = noise_params[:,0:self.latent_dim], noise_params[:,self.latent_dim:]
-        sample = self.sample_noise(mean, logvar)
-        
-        decoded_latent_code = self.latent_code_decoder(sample)
-        decoded_latent_code = decoded_latent_code.reshape(decoded_latent_code.shape[0],16, 9, 27)
+class Decoder(nn.Module):
 
-        optical_flow = self.decoder(decoded_latent_code)
+    def __init__(self, encoded_dim , input_channels= 2, latent_dim=108, out_channels=2, lr=1e-3):
 
-        optical_flow = 5 * torch.tanh(optical_flow)
+        super(Decoder, self).__init__()
 
-        return optical_flow 
+
+        self.latent_code_decoder = nn.Sequential(
+                                        nn.Linear(in_features=latent_dim, out_features=encoded_dim, bias=True),
+                                        nn.ReLU(),
+                                        )
+
+
+        self.decoder = nn.Sequential(
+                            nn.ConvTranspose2d(in_channels=16, out_channels=16, kernel_size=3, stride=2),
+                            nn.ReLU(),
+                            nn.BatchNorm2d(16),
+                            nn.ConvTranspose2d(in_channels=16, out_channels=8, kernel_size=3, stride=2),
+                            nn.ReLU(),
+                            nn.BatchNorm2d(8),
+
+                            nn.ConvTranspose2d(in_channels=8, out_channels=8, kernel_size=3, stride=2),
+                            nn.ReLU(),
+                            nn.BatchNorm2d(8),
+                            nn.ConvTranspose2d(in_channels=8, out_channels=4, kernel_size=3, stride=2),
+                            nn.ReLU(),
+                            nn.BatchNorm2d(4),
+
+                            nn.ConvTranspose2d(in_channels=4, out_channels = 4, kernel_size=2, stride=2),
+                            nn.ReLU(),
+                            nn.BatchNorm2d(4),
+                            nn.ConvTranspose2d(in_channels=4, out_channels = 2, kernel_size=3, stride=1),
+                            )
+
+        self.Decoptimizer = torch.optim.Adam(self.parameters(), lr=lr)
+
+    def forward(self, x):
+        return self.decoder(x)
+
+
 
 
 
